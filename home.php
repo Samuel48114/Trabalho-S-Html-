977b6ea9279177bfa0223076a5e024cb4f3bcf4a
Your Name <you@example.com>
2024-02-29 16:04:20 -0300

    Restaurante

---
+++ home.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+    <title>Reservas</Title>
+</head>
+<body>
+    <!-- logo, nome restaurante -->
+    <!-- Definir, cor fontes e alinhamentos -->
+<form name="formulario" method="post" action="">
+
+Nome:<br>
+<input type ="text" name="nome" id="nome" size=30>
+<br><br>
+Endereço:<br>
+<input type ="text" name="endereco" id="endereco" size=30>
+<br><br>
+cidade:<br>
+<input type ="text" name="cidade" id="cidade" size=30>
+<br><br>
+<select name="estado" id="estado">
+<option value="SC">Santa Catarina
+<option value="PR">Parana
+<option value="RS">Rio Grande do sul
+</select>
+<br><br>
+E-mail:<br>
+<input type ="text" name="email" id="email" size=30>
+<br><br>
+Telefone:<br>
+<input type ="text" name="telefone" id="telefone" size=30>
+<br><br>
+Celular:<br>
+<input type ="text" name="celular" id="celular" size=30>
+<br><br>
+
+
+
+
+
+
+
+
+</body>
+
+
+</html>
